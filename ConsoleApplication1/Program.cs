d3d0fe8de43d5717572ed982ca43268846a551ce
cyrus <rokahaparameshwor2@gmail.com>
2018-09-26 09:40:46 +0545

    initial

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 6;
+            double sum = Add(a, b);
+            Console.WriteLine(sum);
+
+        }
+        static double Add (double a, double b)
+        {
+            return (a + b);
+        }
+    }
+}
